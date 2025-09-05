88827da06f7d1047836d3ba8d70144aa4ef728b5
Valentina Iramain <valentinairamain2020@gmail.com>
2025-09-05 10:32:18 -0300

    Se realiza ejercicio 4

---
+++ js/tp1-js-ejercicio4.js
@@ -0,0 +1,8 @@
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// Ejemplo: 
+
+// input: Coder 
+// Output: Hola Coder
+
+const nombre = prompt('Ingrese el nombre del usuario: ')
+document.writeln('Hola ',nombre)
